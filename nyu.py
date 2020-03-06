d6240fba2ab30ce4267337c45820ad96100941b3
Logan O'Sullivan Bruns <logan@gedanken.org>
2020-03-06 11:21:27 -0500

    Move train, val, test splits into dataset reader

--- nyu.py
+++ nyu.py
@@ -2,7 +2,7 @@
 
 import tensorflow as tf
 
-def NYUv2Dataset(filename):
+def NYUv2Dataset(filename, batch_size=32):
     """ Create a dataset to read NYUv2 dataset """
 
     feature_description = {
@@ -15,10 +15,18 @@ def NYUv2Dataset(filename):
         example = tf.io.parse_single_example(example_proto, feature_description)
         image = example['image']
         image /= 255
+        image = tf.transpose(image, [1, 2, 0])
         depth = example['depth']
         return (image, depth)
 
     dataset = tf.data.TFRecordDataset('data/nyu_depth_v2.tfrecord')
     dataset = dataset.map(_parse_function)
     
-    return dataset
+    train_size = 1200
+    test_size = 125
+
+    train = dataset.take(train_size).cache().shuffle(2500).batch(batch_size)
+    test = dataset.skip(train_size).take(test_size).cache().batch(batch_size)
+    val = dataset.skip(train_size+test_size).cache().batch(batch_size)
+
+    return (train, val, test)
